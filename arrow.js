4b0799b9bfe81cad7b37e924a014c00abebe3eef
draytonne <karldrayton22@gmail.com>
2021-02-04 21:37:46 +0300

    "commit"

--- arrow.js
+++ arrow.js
@@ -33,3 +33,30 @@ document.write(("The factorial of " + n + " is " + number)+"<br>");
 
 const factoriall = (n)=>n==0 ?1:n *factoriall(n-1);
 document.writeln("The factorial of "+ 6+ "=" +factoriall(6)+ "<br>");
+
+const bill = function(products,tax)
+{
+    let total = 0;
+    for(let i=0 ;i< products.length; i++)
+    {
+        total += products[i]+ products[i] * tax;
+    }
+    return total;;
+}
+document.writeln(bill([10,80,56,89,45,30], 0.14),"<br>");
+
+const billd = (products,tax) => products[i]+products[i]*tax;
+document.writeln(billd([10,80,56,89,45,30],0.14) +"<br>");
+
+function power(x,n)
+{
+if (n==0)
+return 1;
+else 
+return x* power(x,n-1);
+}
+
+document.write(power(2,0)+ "<br>"); 
+
+const powerr = (x,n) =>n==0 ?1:x*powerr(x,n-1);
+document.write(powerr(2,3)+ "<br>");
